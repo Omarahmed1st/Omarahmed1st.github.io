c808425d486616357c3e0d259b9b16e3e219911f
Omarahmed1st <140261654+Omarahmed1st@users.noreply.github.com>
2024-06-29 17:44:26 +0300

    Update bootstrap.js

--- module5/js/bootstrap.js
+++ module5/js/bootstrap.js
@@ -3,9 +3,11 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under the MIT license
  */
+
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
+
 +function ($) {
   'use strict';
   var version = $.fn.jquery.split(' ')[0].split('.')
@@ -13,6 +15,7 @@ if (typeof jQuery === 'undefined') {
     throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 3')
   }
 }(jQuery);
+
 /* ========================================================================
  * Bootstrap: transition.js v3.3.6
  * http://getbootstrap.com/javascript/#transitions
