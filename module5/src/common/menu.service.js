6b19253479623416b6f0fe2b250b76b21df9d18b
Omarahmed1st <140261654+Omarahmed1st@users.noreply.github.com>
2024-09-15 15:19:49 +0300

    Update menu.service.js

--- module5/src/common/menu.service.js
+++ module5/src/common/menu.service.js
@@ -8,7 +8,17 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
+  service.user = {};
 
+  service.saveUser = function(user) {
+    service.user = angular.copy(user);
+    console.log(service.user);
+  }
+
+  service.getUser = function() {
+    return service.user;
+  }
+  
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
       return response.data;
@@ -17,7 +27,6 @@ function MenuService($http, ApiPath) {
 
 
   service.getMenuItems = function (category) {
-    console.log('first function', category);
     var config = {};
     if (category) {
       config.params = {'category': category};
@@ -28,28 +37,9 @@ function MenuService($http, ApiPath) {
     });
   };
 
-
-  service.getCategory = function (shortName) {
-    return $http.get(ApiPath + '/categories/' + shortName + '.json').then(function (response) {
-      return response.data;
-    });
-  };
-
-
-  service.getMenuItem = function(shortName) {
-    return $http.get(ApiPath + '/menu_items/' + shortName + '.json')
-    .then(function(response) {
-      return response.data;
-    });
-  };
-
-
-  service.saveMenuItem = function (menuItem) {
-    return $http.put(ApiPath + '/menu_items/' + menuItem.short_name, menuItem)
-    .then(function (response) {
-      return response.data;
-    });
-  };
+  service.getFavoriteDish = function(short_name) {
+    return $http.get(ApiPath + '/menu_items/' + short_name + '.json');
+  }
 
 }
 
